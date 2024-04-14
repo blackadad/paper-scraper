3b155e66ec6755d67b4e5bf2c26517698cb0e6db
blackadad <124536215+blackadad@users.noreply.github.com>
2024-04-14 13:38:27 -0700

    reduced internal _limit to increase test speed

--- tests/test_paperscraper.py
+++ tests/test_paperscraper.py
@@ -199,9 +199,10 @@ class Test0(IsolatedAsyncioTestCase):
 
     async def test_high_limit(self) -> None:
         papers = await paperscraper.a_search_papers(
-            "molecular dynamics", search_type="google", year="2019-2023", limit=25
+            "molecular dynamics", search_type="google", 
+            year="2019-2023", limit=5, _limit=2
         )
-        assert len(papers) > 20
+        assert len(papers) >= 5
 
 
 class TestGS(IsolatedAsyncioTestCase):
